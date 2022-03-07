739e051bf88cb2e5ca0dd94b12cff49139fd4774
Hussain K M Tansire <tansirorick525@gmail.com>
2022-03-07 17:27:13 +0800

    mobile version update

---
+++ src/Components/NavItems.js
@@ -0,0 +1,48 @@
+export const navItems = [
+    {
+      id: 1,
+      title: "Home",
+      path: "./",
+      cName: "nav-item",
+    },
+    {
+      id: 2,
+      title: "Services",
+      path: "./",
+      cName: "nav-item",
+    },
+    {
+      id: 3,
+      title: "Products",
+      path: "./products",
+      cName: "nav-item",
+    },
+    {
+      id: 4,
+      title: "Contact Us",
+      path: "./contactus",
+      cName: "nav-item",
+    },
+  ];
+
+
+export const serviceDropdown = [
+    {
+      id: 1,
+      title: "Exports",
+      path: "/Export",
+      cName: "submenu-item",
+    },
+    {
+      id: 2,
+      title: "Manufacturing",
+      path: "/manufacturing",
+      cName: "submenu-item",
+    },
+    {
+      id: 3,
+      title: "Marketing",
+      path: "/marketing",
+      cName: "submenu-item",
+    },
+  ];
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import logo from '../Assets/Asset 16.png'
+import logo1 from '../Assets/logo icon.png'
+
+import { navItems } from "./NavItems";
+import Dropdown from "./Dropdown";
 
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  
+  const [dropdown, setDropdown] = useState(false); 
+
   const [color, setColor] = useState(false)
+
   const changeColor =()=>{
     if(window.scrollY >= 90){
       setColor(true)
@@ -22,13 +27,35 @@ window.addEventListener('scroll',changeColor)
   const closeMobileMenu = () => setClick(false);
 
 
+  const [logo, setLogo] = useState(false)
+  const changeLogo = ()=>{
+    if(window.scrollY >=90 ){
+      setLogo(true)
+
+    }else{
+      setLogo(false)
+    }
+  }
+  window.addEventListener('scroll',changeLogo)
+
+
+
   
 
   return (
     <>
       <nav className={color ? 'navbar navbar-bg' : 'navbar'}>
         <Link to='/' className='navbar-logo' onClick={closeMobileMenu} >
-          <img src={logo} alt="" srcset="" />
+          
+          <div className={logo ? 'color1': 'color2'}>
+          
+           <div className='textarealogo'>
+           <img src= {logo1 } alt="" srcset="" /> <span className='lt1' > Agroverse</span> <span className='lt2'><br /> Limited</span>
+          </div>
+          </div>
+
+         
+
         </Link>
         <div className='menu-icon' onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -50,6 +77,29 @@ window.addEventListener('scroll',changeColor)
              About
             </Link>
           </ul>
+
+
+          <ul className="nav-item">
+          {navItems.map((item) => {
+            if (item.title === "Services") {
+              return (
+                <ul
+                  key={item.id}
+                  className='nav-item'
+                  onMouseEnter={() => setDropdown(true)}
+                  onMouseLeave={() => setDropdown(false)}
+                >
+                  <Link className='nav-links' to={item.path}>{item.title}</Link>
+                  {dropdown && <Dropdown />}
+                </ul>
+              );
+            }
+           
+          })}
+        </ul>
+
+
+{/* 
           <ul className='nav-item'>
             <Link
               to='/Export'
@@ -77,6 +127,10 @@ window.addEventListener('scroll',changeColor)
              Marketing
             </Link>
           </ul>
+
+           */}
+
+
           <ul className='nav-item'>
             <Link
               to='/contact'
